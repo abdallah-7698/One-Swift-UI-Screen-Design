953ae15ef76b25378e01b7584801df984d1723c6
abdallah.mohammed <abdallah.mohamed@1pass.app>
2025-11-07 00:06:42 +0200

    Initial commit for `VerticalScrollCheckoutViewApp` that contains vertical scroll checkout screen

---
+++ VerticalScrollCheckoutView/VerticalScrollCheckoutView/VerticalScrollCheckoutViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalScrollCheckoutViewApp.swift
+//  VerticalScrollCheckoutView
+//
+//  Created by name on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct VerticalScrollCheckoutViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
